9cdaab2ccb4d3f948d2c57e6b87324bcd8bb17a6
EVAN M. BLUHM <embluhm@us.ibm.com>
2017-03-12 01:39:44 -0800

    Add description to insertion sort

--- sorting/insertionsort.py
+++ sorting/insertionsort.py
@@ -2,6 +2,11 @@
 Worst Case:   O(n^2)
 Best Case:    O(n)
 Average Case: O(n^2)
+Insertion sort
+iterates, consuming one input element each repetition, and growing a sorted
+output list. Each iteration, insertion sort removes one element from the input
+data, finds the location it belongs within the sorted list, and inserts it
+there. It repeats until no input elements remain.
 """
 
 
