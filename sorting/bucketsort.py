b75e2f8b3b6ef0e4d18b1c4a18aa553c908af3ce
EVAN M. BLUHM <embluhm@us.ibm.com>
2017-03-12 20:46:14 -0700

    Remove print statements from bucket sort

--- sorting/bucketsort.py
+++ sorting/bucketsort.py
@@ -19,16 +19,12 @@ from quicksort import quicksort
 
 def bucketsort(input_list):
     biggest = max(input_list)
-    print("\nbiggest: %f" % biggest)
     n_buckets = biggest / 10 + 1
-    print("\nn_buckets: %d" % n_buckets)
     buckets = [list() for _ in range(n_buckets)]
     for idx, x in enumerate(input_list):
         buckets[x / 10].append(x)
-    print("\nbuckets:", buckets)
     for idx, bucket in enumerate(buckets):
         buckets[idx] = quicksort(bucket)
-    print("\nbuckets:", buckets)
     result = []
     for bucket in buckets:
         result += bucket
